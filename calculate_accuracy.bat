0d7d29ab8d5730c1b7e678f5b3bfdc7670862fde
cb3fias <cb3fias@AP704924.ztb.icb.commerzbank.com>
2018-02-09 17:27:15 +0100

    sadfgsad

---
+++ calculate_accuracy.bat
@@ -0,0 +1,2 @@
+P:\GRM-CC\Projekt_BWA_ComTS\20_Rentenbescheide\5_OCR_renten\PortablePython_2.7.6.1\App\python.exe P:\GRM-CC\Projekt_BWA_ComTS\20_Rentenbescheide\5_OCR_renten\rentner_accuracy.py
+pause
