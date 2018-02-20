e76671b5598c16fe6399e9c71c892e8560a757d2
cb3fias <cb3fias@AP704924.ztb.icb.commerzbank.com>
2018-02-20 17:16:24 +0100

    s

--- calculate_accuracy.bat
+++
@@ -1,2 +0,0 @@
-P:\GRM-CC\Projekt_BWA_ComTS\20_Rentenbescheide\5_OCR_renten\PortablePython_2.7.6.1\App\python.exe P:\GRM-CC\Projekt_BWA_ComTS\20_Rentenbescheide\5_OCR_renten\rentner_accuracy.py
-pause
--- primeAnregungen/helloworld.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env python
-#!"C:\Program Files\WinPython-32bit-2.7.5.3\python-2.7.5\python.exe"
-
-print ("Content-Type: text/html\n")
-print ('Hello World XX')
